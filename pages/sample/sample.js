86059d1c502536faf3cea7a53829be2d5fb9267d
Praveen <dpraveen453@gmail.com>
2022-03-03 00:36:15 +0530

    commited changes in introtalk form

--- pages/sample/sample.js
+++
@@ -1,8 +0,0 @@
-export default function FirstPost() {
-    return (
-        <div>
-            <h1>Sample Page</h1>
-            <Link href="/"><a>return to home page</a></Link>
-        </div>
-    )
-}
